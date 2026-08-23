0814d07eab74503e7a7b1a878abf1304b5d5d11d
Camilo Hurtado <camilohurtadocarvajal25@gmail.com>
2026-08-23 14:47:29 -0500

    FZ-002:  bootstrap Spring Boot application

---
+++ backend/src/main/java/com/freezhub/FreezeHubApplication.java
@@ -0,0 +1,13 @@
+package com.freezhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreezeHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FreezeHubApplication.class, args);
+    }
+
+}
